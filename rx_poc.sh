d9b8bd2fcd2100349a7bc55c93f14954044389a5
beyondmayOwO <78870914+beyondmayOwO@users.noreply.github.com>
2024-10-02 23:06:52 +0630

    Extract the forecast temperature for noon tomorrow

--- rx_poc.sh
+++ rx_poc.sh
@@ -7,3 +7,7 @@ city=Casablanca
 #To extract Current Temperature
 obs_temp=$(curl -s wttr.in/$city?T | grep -Pm 1 "°[CF]" | grep -Po "[+-]?\d+(?=[\(\s])")
 echo "The current temperature of $city: $obs_temp"
+
+# To extract the forecast temperature for noon tomorrow
+fc_temp=$(curl -s wttr.in/$city?T | head -23 | tail -1 | cut -d 'C' -f2 | grep -Po "[+-]?\d+(?=[\(\s])")
+echo "The forecast temperature for noon tomorrow for $city: $fc_temp"
