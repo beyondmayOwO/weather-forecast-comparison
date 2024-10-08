cbeafc64019539e39e1b06a2164197c7d0f94d7c
beyondmayOwO <78870914+beyondmayOwO@users.noreply.github.com>
2024-10-09 01:08:06 +0630

    Calculate the accuracy range then assign the label

--- fc_accuracy.sh
+++ fc_accuracy.sh
@@ -6,16 +6,34 @@ echo "Today's temperature is $today_temp"
 forecasted_for_today=$(tail -2 rx_poc.log | head -1 | cut -d " " -f5)
 echo "Forecasted temperature for today is $forecasted_for_today"
 
-# Calculate the forecast accuracy by calculating the difference between today's temperature and forecasted temperature
+# Calculate the difference between today and forecasted temperature
+accuracy_range=$(($today_temp-$forecasted_for_today))
+# Make it positive if it's negative
+[ "$accuracy_range" -lt 0 ] && accuracy_range=$((-$accuracy_range))
+
+# Calculate the accuracy range then assign the label
+if [ "$accuracy_range" -le 1 ]
+then
+  accuracy_label=excellent
+elif [ "$accuracy_range" -eq 2 ]
+then
+  accuracy_label=good
+elif [ "$accuracy_range" -eq 3 ]
+then
+  accuracy_label=fair
+elif [ "$accuracy_range" -ge 4 ]
+then
+  accuracy_label=poor
+fi
+
+# Calculate the forecast accuracy
 if [ "$today_temp" -eq "$forecasted_for_today" ]
 then
-  echo "Today's temperature is the same as the forecasted temperature."
+  echo "Today's temperature is the same as the forecasted temperature. Forecast accuracy is $accuracy_label."
 elif [ "$today_temp" -gt "$forecasted_for_today" ]
 then
-  hotter_temp=$(($today_temp-$forecasted_for_today))
-  echo "Today's temperature is hotter by $hotter_temp"
+  echo "Today's temperature is hotter by $accuracy_range. Forecast accuracy is $accuracy_label."
 elif [ "$today_temp" -lt "$forecasted_for_today" ]
 then
-  colder_temp=$(($forecasted_for_today-$today_temp))
-  echo "Today's temperature is colder by $colder_temp"
+  echo "Today's temperature is colder by $accuracy_range. Forecast accuracy is $accuracy_label."
 fi
