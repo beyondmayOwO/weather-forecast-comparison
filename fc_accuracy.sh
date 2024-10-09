31be6eae4ae4950de003c5bdb6eb0abccfb49e55
beyondmayOwO <78870914+beyondmayOwO@users.noreply.github.com>
2024-10-09 23:28:45 +0630

    Extract the current day, month, and year and append the record

--- fc_accuracy.sh
+++ fc_accuracy.sh
@@ -1,13 +1,20 @@
+#! /bin/bash
+
+# Extract the current day, month, and year
+year=$(tail -1 rx_poc.log | cut -d " " -f1)
+month=$(tail -1 rx_poc.log | cut -d " " -f2)
+day=$(tail -1 rx_poc.log | cut -d " " -f3)
+
 # Extract Today's current Temperature
-today_temp=$(tail -1 rx_poc.log | cut -d " " -f4)
-echo "Today's temperature is $today_temp"
+obs_temp=$(tail -1 rx_poc.log | cut -d " " -f4)
+echo "Today's temperature is $obs_temp"
 
 # Extract the forecasted temprature for today
 forecasted_for_today=$(tail -2 rx_poc.log | head -1 | cut -d " " -f5)
 echo "Forecasted temperature for today is $forecasted_for_today"
 
 # Calculate the difference between today and forecasted temperature
-accuracy_range=$(($today_temp-$forecasted_for_today))
+accuracy_range=$(($obs_temp-$forecasted_for_today))
 # Make it positive if it's negative
 [ "$accuracy_range" -lt 0 ] && accuracy_range=$((-$accuracy_range))
 
@@ -27,13 +34,17 @@ then
 fi
 
 # Calculate the forecast accuracy
-if [ "$today_temp" -eq "$forecasted_for_today" ]
+if [ "$obs_temp" -eq "$forecasted_for_today" ]
 then
   echo "Today's temperature is the same as the forecasted temperature. Forecast accuracy is $accuracy_label."
-elif [ "$today_temp" -gt "$forecasted_for_today" ]
+elif [ "$obs_temp" -gt "$forecasted_for_today" ]
 then
   echo "Today's temperature is hotter by $accuracy_range. Forecast accuracy is $accuracy_label."
-elif [ "$today_temp" -lt "$forecasted_for_today" ]
+elif [ "$obs_temp" -lt "$forecasted_for_today" ]
 then
   echo "Today's temperature is colder by $accuracy_range. Forecast accuracy is $accuracy_label."
 fi
+
+# Append the record to the historical forecast accuracy file
+record=$(echo -e "$year\t$month\t$day\t$obs_temp\t$forecasted_for_today\t$accuracy_label\t$accuracy_range")
+echo $record >> historical_fc_accuracy.tsv
